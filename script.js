c2dce1f0baa4f97776a8bad04f693ebae5ce82ee
Rabarbara2 <118020475+Rabarbara2@users.noreply.github.com>
2025-10-07 09:12:45 +0000

    C 1_1.1 kotek

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 (function () {
-  //TODO0
+  document.getElementById("ex1_button").addEventListener("click", function () {
+    document.getElementById("ex1_content").innerHTML = "0,1,2,3,4,5,6,7,8,9";
+  })
 })();
